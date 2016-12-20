8be357509ccd3d1ab8961a2b0263007ff0463e51
Enrico Masala <masala@polito.it>
2016-12-20 07:51:55 +0100

    Updated with reference to scripts in subfolders

--- QualityMeasure.bat
+++ QualityMeasure.bat
@@ -17,10 +17,12 @@ python ./SoftwareLibraries/AggregatePSNRtoCSV.py
 ::################# Quality Measure Analysis #########################
 ::The Quality Measure Analysis module saves data in DATA\QMA
 
-
+( cd SoftwareLibraries ; bash script_run_all.sh )
 
 ::###################### Visualize ###################################
 ::The visualize module takes data from DATA\QMA
+
+( cd DATA/QMA; gnuplot graph_scatter__PSNRvar_vs_PSNR_interpolated.gplot )
 
 
 
